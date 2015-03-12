745729865e746135690f47720ae4402e1bd95088
Patrick Murphy <murphypatv@gmail.com>
2015-03-12 16:38:57 +0000

    Add a base class to hold frequently used methods

--- src/wit/cc/activities/Home.java
+++ src/wit/cc/activities/Home.java
@@ -1,21 +1,11 @@
 package wit.cc.activities;
 
-import java.util.List;
-
 import wit.cc.R;
 import wit.cc.adapters.RouteAdapter;
-import wit.cc.models.Route;
-import wit.cc.models.RouteData;
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class Home extends ListActivity {
+public class Home extends Base {
 	
-	List<Route> routes = new RouteData().getRoutes(); // get hard coded data from RouteData.java
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +14,5 @@ public class Home extends ListActivity {
 		
 		RouteAdapter adapter = new RouteAdapter(this, R.layout.item_route, routes);
 		setListAdapter(adapter);
-		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.optionsmenu, menu); // inflate menu
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		
-		switch (item.getItemId()) {
-		case R.id.help:
-			
-			break;
-		case R.id.home:
-			
-			break;
-
-		default:
-			break;
-		}
-		
-		return super.onOptionsItemSelected(item);
-	}
-	
-	
 }
