a3dfaf55e1dfb77f1fa229d7ed8319a4a03f03df
Patrick Murphy <murphypatv@gmail.com>
2015-03-12 16:29:17 +0000

    Edit styles to display action bar with app icon

--- src/wit/cc/activities/Home.java
+++ src/wit/cc/activities/Home.java
@@ -29,20 +29,25 @@ public class Home extends ListActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.optionsmenu, menu); // inflate menu
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		switch (item.getItemId()) {
+		case R.id.help:
+			
+			break;
+		case R.id.home:
+			
+			break;
+
+		default:
+			break;
 		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 	
