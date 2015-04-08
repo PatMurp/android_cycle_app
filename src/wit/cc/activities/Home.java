6e0b702ed48129cf40e7cdd9e7a73c7439255b3b
Patrick Murphy <murphypatv@gmail.com>
2015-04-08 16:24:24 +0100

    add totals screen

--- src/wit/cc/activities/Home.java
+++ src/wit/cc/activities/Home.java
@@ -1,6 +1,7 @@
 package wit.cc.activities;
 
 import wit.cc.R;
+import wit.cc.models.Route;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,8 @@ public class Home extends Base {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		//setupRoutes();
 	}
 	
 	@Override
@@ -24,5 +27,15 @@ public class Home extends Base {
 
 	public void add(View v) {
 		goToActivity(this, Add.class, null);
+	}
+	
+	public void totals(View v) {
+		goToActivity(this, Totals.class, null);
+	}
+	
+	public void setupRoutes() {
+		routeList.add(new Route("03-04-15", 7.5, "A2"));
+		routeList.add(new Route("06-04-15", 6.3, "B2"));
+		routeList.add(new Route("10-04-15", 6.1, "A4"));
 	}
 }
