b1fea47887141887e5984d44d1dacfa9dd23bc69
Patrick Murphy <murphypatv@gmail.com>
2015-04-09 10:59:18 +0100

    display total distance

--- src/wit/cc/activities/Totals.java
+++ src/wit/cc/activities/Totals.java
@@ -1,15 +1,34 @@
 package wit.cc.activities;
 
 import wit.cc.R;
+import wit.cc.models.Route;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Totals extends Base {
-
+	private double tDistance = 0.0;
+	private double tCo2Saving = 0.0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_totals);
+		
+		// itterate through route array
+		for (Route route: routeList) {
+			tDistance += route.getDistance(); // total distance
+		}
+		
+		TextView totalDistance = (TextView) findViewById(R.id.totalDistance);
+		totalDistance.setText(String.format("%.02f km", tDistance));
+		
 	}
-
 	
+	
+	
+
+
 }
+
+
